ae9387b9e54f737bd0cb91ee517d38355a3837c1
PhiNotPi <mrclarinetplayer@gmail.com>
2015-10-09 21:42:12 -0400

    Update controller.pl

--- controller.pl
+++ controller.pl
@@ -4,6 +4,6 @@ use warnings;
 my $filename = 'report.txt';
 open(my $fh, '>>', $filename) or die "Could not open file '$filename' $!";
 sleep(10);
-print $fh "This controller simply waits 10 seconds and appends a line.\n";
+print $fh "This controller simply waits 10 seconds and appends a line each time it is run.\n";
 close $fh;
 print "done\n";
